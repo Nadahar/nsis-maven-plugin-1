a453e23fc84d89efbec779679064a74920ec17ee
Jaguaraquem A. Reinaldo <jaguar.adler@gmail.com>
2016-02-02 23:22:57 -0200

    Added Compressor enum class that define compressor types. Only lzma was added

---
+++ src/main/java/br/eti/jadler/nsis/maven/plugin/Compressor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.eti.jadler.nsis.maven.plugin;
+
+/**
+ *
+ * @author Jaguaraquem A. Reinaldo <jaguar.adler@gmail.com.br>
+ */
+public enum Compressor {
+    
+    LZMA;
+}
