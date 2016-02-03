5dec2ee9209796b466c2a3f5b8fc038c96b8062f
Jaguaraquem A. Reinaldo <jaguar.adler@gmail.com>
2016-02-02 23:23:43 -0200

    Defined some plugins parameters

--- src/main/java/br/eti/jadler/nsis/maven/plugin/Generate.java
+++ src/main/java/br/eti/jadler/nsis/maven/plugin/Generate.java
@@ -5,6 +5,8 @@
  */
 package br.eti.jadler.nsis.maven.plugin;
 
+import java.io.File;
+import java.net.URL;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -15,13 +17,67 @@ import org.apache.maven.project.MavenProject;
 
 /**
  *
- * @author Jaguaraquem A. Reinaldo <jaguaraquem.adler@biologica.com.br>
+ * @author Jaguaraquem A. Reinaldo <jaguar.adler@gmail.com.br>
  */
 @Mojo(name = "generate", defaultPhase = LifecyclePhase.PREPARE_PACKAGE)
 public class Generate extends AbstractMojo {
 
-    @Parameter(property = "${project}", required = true, readonly = true)
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
+
+    @Parameter(defaultValue = "${project.basedir}", readonly = true)
+    private File basedir;
+
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true)
+    private File target;
+
+    @Parameter
+    private String installRoot;
+    
+    @Parameter(property = "nsis.muiIcon")
+    private String icon;
+    
+    @Parameter(property = "nsis.muiUnIcon")
+    private String unIcon;
+    
+    @Parameter
+    private String extraPreInstallCommand;
+    
+    @Parameter
+    private String extraInstallCommand;
+    
+    @Parameter
+    private String extraUninstallCommand;
+    
+    @Parameter
+    private Boolean enableUninstallBeforeInstall;
+    
+    @Parameter
+    private Boolean modifyPath;
+    
+    @Parameter
+    private String displayName;
+    
+    @Parameter
+    private String packageName;
+    
+    @Parameter
+    private URL helpLink;
+    
+    @Parameter
+    private URL urlInfoAbout;
+    
+    @Parameter
+    private String contact;
+    
+    @Parameter
+    private String installDirectory;
+    
+    @Parameter
+    private String vendor;
+    
+    @Parameter
+    private File outputFileName;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
